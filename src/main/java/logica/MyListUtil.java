daaef60390e3ebed16d7db7dadb7fb250b599e41
Singh <satwinder.b.singh@accenture.com>
2020-11-04 11:56:47 +0100

    commit senza date

---
+++ src/main/java/logica/MyListUtil.java
@@ -0,0 +1,63 @@
+package logica;
+
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyListUtil {
+
+	//non so se vuole un ordinamento fatto da funzione oppure fatto maunalmente, io lo faccio manunalmente 
+	//so che non è efficiente , ma credo che non posso andare a vedere come si setta il compartore(che non ricordo)
+   ArrayList<Integer> l = new ArrayList<Integer>();
+   int tmp=0;
+   
+   public  List<Integer> ordina (ArrayList<Integer> l, String verso )
+   {
+	   if(verso.equals("Decrescente"))
+	   {
+		   for(int i=0;i<l.size();i++)
+		   {
+			   for(int j=0;j<l.size();j++)
+			   {
+				   if(l.get(i)>l.get(j))
+				   {
+					   tmp=l.get(j);
+					   l.set(j, l.get(i));
+					   l.set(i, tmp);
+				   }
+			   }
+		   }
+	   }if(verso.equals("Crescente"))
+	   {
+		   for(int i=0;i<l.size();i++)
+		   {
+			   for(int j=0;j<l.size();j++)
+			   {
+				   if(l.get(i)<l.get(j))
+				   {
+					   tmp=l.get(j);
+					   l.set(j, l.get(i));
+					   l.set(i, tmp);
+				   }
+			   }
+		   }
+	   }
+	   
+	return l;
+	   
+   }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			MyListUtil m =new MyListUtil();
+			ArrayList<Integer > l= new ArrayList<Integer>();
+			l.add(1);
+			l.add(3);
+			l.add(10);
+			l.add(21);
+			System.out.println(m.ordina(l, "Decrescente")); 
+			System.out.println(m.ordina(l, "Crescente")); 
+
+	}
+
+}
---
+++ src/test/java/provaINGSE/TestMyListUtil.java
@@ -0,0 +1,69 @@
+package provaINGSE;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import logica.MyListUtil;
+
+public class TestMyListUtil {
+
+	private static MyListUtil l;
+	
+	static ArrayList<Integer> list1 ;
+	static ArrayList<Integer> list;
+	@BeforeClass
+	public static void preparaTest() {
+			System.out.println("ciao inizio");
+			l=new MyListUtil();
+			
+			list1 = new ArrayList<Integer>();
+			
+			 list = new ArrayList<Integer>();
+			
+			list.add(1);
+			list.add(20);
+			list.add(30);
+			list.add(12);
+			list.add(4);
+		//Print orario inizio;
+	}
+	@Test
+	public   void testOrdinaCrescente()
+	{
+		
+		list1.add(1);
+		list1.add(4);
+		list1.add(12);
+		list1.add(20);
+		
+		list1.add(30);
+		assertEquals(list1,  l.ordina(list, "Crescente"));
+		 list1.clear();
+	}
+	@Test
+	public void testOrdinaDecrescente()
+	{
+		list1.add(30);
+		list1.add(20);
+		list1.add(12);
+		list1.add(4);
+		list1.add(1);
+		assertEquals(list1,  l.ordina(list,  "Decrescente"));
+
+		
+		
+	}
+		
+	@AfterClass
+	public static void fineTest()
+	{
+		//stampa orario fine
+		System.out.println("Ho finito ");
+	}
+}
